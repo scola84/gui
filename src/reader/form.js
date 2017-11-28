2ac1312e950628e9308452f67664d83152c5baaf
M.C. van den Boogaart <scola@kvdb.name>
2017-11-28 21:37:15 +0100

    Prevent form to be sent multiple times (and reset after response)

--- src/reader/form.js
+++ src/reader/form.js
@@ -4,15 +4,23 @@ import serializeForm from 'form-serialize';
 
 export default class FormReader extends Worker {
   act(route) {
-    select(route.node)
+    const form = select(route.node)
       .select('form')
-      .on('submit', (datum, index, nodes) => {
+      .on('submit', () => {
         event.preventDefault();
-        this._submit(route, nodes[0]);
+        this._submit(route, form);
       });
   }
 
   _submit(route, form) {
+    if (form.attr('action') === '/') {
+      return;
+    }
+
+    form = form
+      .attr('action', '/')
+      .node();
+
     this.pass(route, serializeForm(form, {
       empty: true,
       hash: true
--- src/transformer/response.js
+++ src/transformer/response.js
@@ -1,12 +1,19 @@
 import { Worker } from '@scola/worker';
+import { select } from 'd3';
 
 export default class ResponseTransformer extends Worker {
   act(response, data, callback) {
+    const route = response.request.box;
+
+    select(route.node)
+      .select('form')
+      .attr('action', null);
+
     if (response.status >= 300) {
-      const error = new Error(String(response.status));
-      this.fail(response.request.box, error, callback);
-    } else {
-      this.pass(response.request.box, data, callback);
+      this.fail(route, new Error(String(response.status)), callback);
+      return;
     }
+
+    this.pass(route, data, callback);
   }
 }
