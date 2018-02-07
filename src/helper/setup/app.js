67d558bf9a742253f325954c40e0f1513903e269
M.C. van den Boogaart <scola@kvdb.name>
2018-02-07 23:42:55 +0100

    Remove win as option

--- src/helper/setup/app.js
+++ src/helper/setup/app.js
@@ -1,8 +1,1 @@
-import { select } from 'd3';
-
-export default function setupApp() {
-  select('.app').classed('win', () => {
-    const win = localStorage.getItem('app.win');
-    return win === null || Number(win) === 1;
-  });
-}
+export default function setupApp() {}
