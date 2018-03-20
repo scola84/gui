e879a077dbaa26ab5ce51e8044a3ef7cc4f6831a
M.C. van den Boogaart <scola@kvdb.name>
2018-03-20 16:05:35 +0100

    Do not assume button existence

--- src/helper/fix/submit.js
+++ src/helper/fix/submit.js
@@ -2,7 +2,9 @@ export default function fixSubmit(panel) {
   const button = panel
     .select('.bar.header button[form]');
 
-  if (button.node().form === null) {
+  const node = button.node();
+
+  if (node && node.form === null) {
     button.on('click', () => {
       panel
         .select('#' + button.attr('form'))
