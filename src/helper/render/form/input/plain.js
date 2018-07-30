d7a17f95c8006d5ccad143835de0fe743300eef4
M.C. van den Boogaart <scola@kvdb.name>
2018-07-30 16:45:47 +0200

    Allow plain value as function

--- src/helper/render/form/input/plain.js
+++ src/helper/render/form/input/plain.js
@@ -1,9 +1,15 @@
 export default class Plain {
   render(datum, index, node, format) {
+    let text = format('value');
+
+    if (typeof datum.value === 'function') {
+      text = datum.value(datum);
+    }
+
     node
       .select('.input')
       .append('div')
       .classed('plain', true)
-      .text(format('value') || '-');
+      .text(text || '-');
   }
 }
