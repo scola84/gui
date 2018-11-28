651c0548a8ebac2ec4bed98edf1044aa8b3ecb65
M.C. van den Boogaart <scola@kvdb.name>
2018-11-28 20:00:47 +0100

    Add option to link submit

--- src/helper/render/form/input/submit.js
+++ src/helper/render/form/input/submit.js
@@ -1,3 +1,5 @@
+import { select } from 'd3';
+
 export default class SubmitInput {
   render(datum, index, node, format) {
     const wrap = node
@@ -5,9 +7,25 @@ export default class SubmitInput {
       .append('div')
       .classed('wrap', true);
 
-    return wrap
+    const input = wrap
       .append('input')
       .attr('type', 'submit')
-      .attr('value', format('value'));
+      .attr('value', format('value'))
+      .on('click', () => {
+        this._changeLink(datum, input);
+      });
+
+    return input;
+  }
+
+  _changeLink(datum, input) {
+    if (!datum.link) {
+      return;
+    }
+
+    const link = select(input.node().form)
+      .select(`*[name=${datum.link}]`);
+
+    link.property('value', datum.name);
   }
 }
