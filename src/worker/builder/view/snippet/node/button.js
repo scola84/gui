45fe8c63c4554975af0e51b9f58e46810632c8fb
M.C. van den Boogaart <scola@kvdb.name>
2019-06-22 16:11:21 +0200

    Fix form attribute in button

--- src/worker/builder/view/snippet/node/button.js
+++ src/worker/builder/view/snippet/node/button.js
@@ -1,3 +1,4 @@
+import { event, select } from 'd3';
 import { Node } from '../node';
 
 export class Button extends Node {
@@ -9,5 +10,29 @@ export class Button extends Node {
         type: 'button'
       })
       .name('button');
+  }
+
+  removeBefore() {
+    this._node.on('click.scola-dom', null);
+    this.removeOuter();
+  }
+
+  resolveAfter(box, data) {
+    const form = this._node.attr('form');
+
+    if (typeof form !== 'undefined') {
+      return this.resolveForm(box, data, form);
+    }
+
+    return this._node;
+  }
+
+  resolveForm(box, data, form) {
+    this._node.on('click.scola-dom', () => {
+      event.preventDefault();
+      select('#' + form).dispatch('submit');
+    });
+
+    return this._node;
   }
 }
