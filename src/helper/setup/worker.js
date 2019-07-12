820b6a48c8b50b8430f01f3bf6c976b91483891f
M.C. van den Boogaart <scola@kvdb.name>
2019-07-12 12:32:51 +0200

    Set worker log

--- src/helper/setup/worker.js
+++ src/helper/setup/worker.js
@@ -1,11 +1,18 @@
 import {
+  Worker
+} from '@scola/worker';
+
+import {
   Axis,
   Plot,
   ViewBuilder
 } from '../../worker';
 
 export function worker() {
+  Worker.setLog(Worker.log);
+
   ViewBuilder.setup();
+
   Axis.setup();
   Plot.setup();
 }
